edeb4586e72437af9fb8367047573a7e08e6ac04
Ravi Anand <ravianand291@gmail.com>
2024-01-15 12:31:07 +0530

    build issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <h1>Operations - /operations</h1>
       <h1>Patient - /patient</h1>
       <h1>Physio - /physio</h1>
+      <Link to="/physio">Hi</Link>
     </div>
   );
 };
